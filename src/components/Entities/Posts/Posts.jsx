5660b5280c90c2f6ecd7586e424c00bc6650a2ec
visneviySecret <visneviy.secret@yandex.ru>
2023-05-14 20:53:44 +0300

    add skeleton to app page

--- src/components/Entities/Posts/Posts.jsx
+++ src/components/Entities/Posts/Posts.jsx
@@ -4,9 +4,10 @@ import { List } from '@mui/material'
 import { getPostsWithAuthors, getQuery } from './utils'
 import { getAuthors } from '../Author/api'
 import Post from '../../Shared/Post/Post'
-
+import Loader from '../Comments/Loader'
 function Posts({ user }) {
     const [posts, setPosts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         const query = getQuery(user)
@@ -15,18 +16,28 @@ function Posts({ user }) {
 
     const fetchPosts = async (query) => {
         try {
+            setIsLoading(true)
             const fetchedPosts = await getPosts(query)
             const authors = await getAuthors()
             const postsWithAuthors = getPostsWithAuthors(fetchedPosts, authors)
             setPosts(postsWithAuthors)
+            setIsLoading(false)
         } catch {
             throw new Error(`Can not get posts response from server`)
         }
     }
 
+    if (isLoading) {
+        return <Loader />
+    }
+
     return (
         <List
-            sx={{ width: '100%', maxWidth: 700, bgcolor: 'background.paper' }}
+            sx={{
+                width: '100%',
+                maxWidth: 700,
+                bgcolor: 'background.paper',
+            }}
         >
             {posts.map((post, index) => (
                 <Post post={post} index={index} key={post.id} />
--- src/components/Entities/SelectUser/SelectUser.jsx
+++ src/components/Entities/SelectUser/SelectUser.jsx
@@ -3,9 +3,11 @@ import Select from 'react-select'
 import { getUsers } from './api'
 import { getOptionsFromUserArray } from './utils'
 import { useAlert } from 'react-alert'
+import { Skeleton } from '@mui/material'
 
 function SelectUser({ setUser }) {
     const [users, setUsers] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
     const alert = useAlert()
 
     useEffect(() => {
@@ -14,9 +16,11 @@ function SelectUser({ setUser }) {
 
     const fetchUsers = async () => {
         try {
+            setIsLoading(true)
             const fetchedUsers = await getUsers('')
             const formattedUsers = getOptionsFromUserArray(fetchedUsers)
             setUsers(formattedUsers)
+            setIsLoading(false)
         } catch {
             throw new Error('Can not get users respones from server')
         }
@@ -25,6 +29,17 @@ function SelectUser({ setUser }) {
     const handleSelect = (user) => {
         alert.info(`Select user: ${user.label}`)
         setUser(user)
+    }
+
+    if (isLoading) {
+        return (
+            <Skeleton
+                variant="rectangular"
+                width="100%"
+                height={30}
+                sx={{ mb: '20px' }}
+            ></Skeleton>
+        )
     }
 
     return (
