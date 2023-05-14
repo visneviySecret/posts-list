2aba2ddccce8e4c14718e3e09391b2657ee23415
visneviySecret <visneviy.secret@yandex.ru>
2023-05-15 01:53:24 +0300

    fix rollup build

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     base: '/posts-list/',
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            external: ['react-dom/client'],
+        },
+    },
 })
