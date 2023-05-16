986e19d77ab4f3869055f3bcd6abe3bb4ddf72d0
visneviySecret <visneviy.secret@yandex.ru>
2023-05-16 05:20:05 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider, Route } from 'react-router-dom'
 import { router } from './router'
 import { Provider as AlertProvider } from 'react-alert'
 import { AlertTemplate, options } from './components/Shared/Alerts/Alerts'
+import App from './components/Pages/App/App'
+import PostPage from './components/Pages/PostPage/PostPage'
 
 const Root = () => (
     <React.StrictMode>
         <AlertProvider template={AlertTemplate} {...options}>
-            <RouterProvider router={router} />
+            <RouterProvider router={router}>
+                <Route exact path="/" component={App} />
+                <Route exact path="/post/:postId" component={PostPage} />
+            </RouterProvider>
         </AlertProvider>
     </React.StrictMode>
 )
--- src/router.jsx
+++ src/router.jsx
@@ -1,17 +1,9 @@
 import App from './components/Pages/App/App.jsx'
 import ErrorPage from './components/Pages/Error/Error.jsx'
 import PostPage from './components/Pages/PostPage/PostPage.jsx'
-import { createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        errorElement: <ErrorPage />,
-    },
-    {
-        path: '/post/:postId',
-        element: <PostPage />,
-        errorElement: <ErrorPage />,
-    },
-])
+const history = createBrowserHistory()
+
+export const router = <BrowserRouter history={history} />
