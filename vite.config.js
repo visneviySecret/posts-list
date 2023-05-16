a3fad8e6cd703d9df867a363733abeaa58f31a5a
visneviySecret <visneviy.secret@yandex.ru>
2023-05-16 07:14:11 +0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     base: '/posts-list',
     plugins: [react()],
-    // build: {
-    //     rollupOptions: {
-    //         external: ['react-dom/client'],
-    //     },
-    // },
 })
